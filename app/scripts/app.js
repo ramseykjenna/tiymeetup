b2a8ff2ae969185c2e94288615439256b208e015
Jenna Ramsey <jennaramsey@Jennas-MacBook-Air.local>
2016-03-12 13:28:31 -0600

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tiymeetupApp
+ * @description
+ * # tiymeetupApp
+ *
+ * Main module of the application.
+ */
+angular.module('tiymeetupApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tiymeetupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tiymeetupApp
+ */
+angular.module('tiymeetupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
